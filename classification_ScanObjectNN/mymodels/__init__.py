27cd0472d664dffeb1b5bf967be23ff19af7b6d9
chenfeiyang <chenfeiyang@mail.ustc.edu.cn>
2023-01-11 19:58:35 +0800

    my_pointmlp_V1, modify LocalGrouper

---
+++ classification_ScanObjectNN/mymodels/__init__.py
@@ -0,0 +1,5 @@
+from __future__ import absolute_import
+
+# from .my_pointmlp_V1 import pointMLP, pointMLPElite
+# from .my_pointmlp_V2 import pointMLP, pointMLPElite
+from .my_pointmlp_V3 import pointMLP, pointMLPElite
