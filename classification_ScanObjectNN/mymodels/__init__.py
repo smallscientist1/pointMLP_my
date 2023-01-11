e5e8a2ec1cd7b3846fbb1437f3b3ae2e243f3f3c
chenfeiyang <chenfeiyang@mail.ustc.edu.cn>
2023-01-11 20:07:23 +0800

    my_pointmlp_V3, modify LocalAggreation, using KNN

--- classification_ScanObjectNN/mymodels/__init__.py
+++ classification_ScanObjectNN/mymodels/__init__.py
@@ -1,5 +1,6 @@
 from __future__ import absolute_import
 
+# from .pointmlp import pointMLP, pointMLPElite
 # from .my_pointmlp_V1 import pointMLP, pointMLPElite
 # from .my_pointmlp_V2 import pointMLP, pointMLPElite
 from .my_pointmlp_V3 import pointMLP, pointMLPElite
